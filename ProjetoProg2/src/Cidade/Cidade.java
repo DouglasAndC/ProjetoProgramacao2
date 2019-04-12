1891d2328a0245fc3fa4325aa1e4f472eb294d0e
mateusmosquera <mateusmosquera@hotmail.com>
2019-04-12 19:40:29 -0300

    Construtor get e set classe cidade

--- ProjetoProg2/src/Cidade/Cidade.java
+++ ProjetoProg2/src/Cidade/Cidade.java
@@ -10,5 +10,48 @@ package Cidade;
  * @author Douglas
  */
 public class Cidade {
+    private long id;
+    private String nome;
+    private String estado;
+    private String pais;
+    private long populacao;
+    
+    public Cidade(){
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+        this.pais = pais; 
+        this.populacao = populacao;
+    }
+     public void setId(int id){
+        this.id = id;
+    }
+    public long getId(){
+        return this.id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+    public String getEstado(){
+        return this.estado;
+    }
+    public void setPais(String pais){
+        this.pais = pais;
+    }
+    public String getPais(String pais){
+        return this.pais;
+    }
+    public void setPopulacao(long populacao){
+        this.populacao = populacao;
+    }
+    public long getPopulacao(){
+        return this.populacao;
+    }
     
 }
