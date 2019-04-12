d97c62fdaad71d7e7dd6ffb10db99642364df188
Douglas Andrade <sousaandradedouglas@gmail.com>
2019-04-12 20:19:21 -0300

    apagando comentarios

--- ProjetoProg2/src/Projeto/Aplicativo/Aplicativo.java
+++ ProjetoProg2/src/Projeto/Aplicativo/Aplicativo.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Projeto.Aplicativo;
 
-/**
- *
- * @author Douglas
- */
+
 public class Aplicativo {
     private long id;
     private String nome;
