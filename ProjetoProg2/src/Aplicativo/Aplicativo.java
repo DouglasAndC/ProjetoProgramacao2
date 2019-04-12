1c10060a2a6ffb29891a1e0ae5975c8ce8847c6d
Douglas Andrade <sousaandradedouglas@gmail.com>
2019-04-12 19:36:41 -0300

    Contrutor e gets e sets classe aplicativo

--- ProjetoProg2/src/Aplicativo/Aplicativo.java
+++ ProjetoProg2/src/Aplicativo/Aplicativo.java
@@ -10,5 +10,39 @@ package Aplicativo;
  * @author Douglas
  */
 public class Aplicativo {
+    private long id;
+    private String nome;
+    private String desenvolvedor;
+    private int numeroDownloads;
     
+    public Aplicativo(long id, String nome, String desenvolvedor, int numeroDownloads){
+        this.id=id;
+        this.nome=nome;
+        this.desenvolvedor=desenvolvedor;
+        this.numeroDownloads=numeroDownloads;
+    }
+    public long getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getDesenvolvedor(){
+        return this.desenvolvedor;
+    }
+    public void setDesenvolvedor(String desenvolvedor){
+        this.desenvolvedor=desenvolvedor;
+    }
+    public int getNumeroDownloads(){
+        return this.numeroDownloads;
+    }
+    public void setNumeroDownloads(int numeroDownloads){
+        this.numeroDownloads=numeroDownloads;
+    }
 }
