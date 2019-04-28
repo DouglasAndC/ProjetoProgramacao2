0d0023add7e08053c624f4575df84438cfb8e4c6
mateusmosquera <mateusmosquera@hotmail.com>
2019-04-28 15:58:08 -0300

    alterando para padrão dao mais coeso

--- ProjetoProg2/src/Projeto/Cidade/DataAccessObjectDB.java
+++ ProjetoProg2/src/Projeto/Cidade/DataAccessObjectDB.java
@@ -10,44 +10,51 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
  *
  * @author mateus
  */
-public class DataAccessObjectDB implements DataAccessObject{
-
-    public Connection getConnection() {
-        String url = "jdbc:derby://localhost:1527/ProjetoProgramacaoSistemas";
-        String user = "projeto";
-        String password = "projeto";
+public class DataAccessObjectDB implements DataAccessObject {
+    private PreparedStatement pstmC;
+    private PreparedStatement pstmR;
+    private PreparedStatement pstmRall;
+    private PreparedStatement pstmU;
+    private PreparedStatement pstmD;
+    private Connection conn;
+    
+    public DataAccessObjectDB() {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            Connection conn = DriverManager.getConnection(url, user, password);
-            return conn;
-        } catch (Exception ex) {
+            this.conn = DriverManager.getConnection("jdbc:derby://localhost:1527/ProjetoProgramacaoSistemas","projeto","projeto");
+             this.pstmC = this.conn.prepareStatement("INSERT INTO Cidade (id,nome,estado,pais,populacao) values (?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);        
+            this.pstmR = this.conn.prepareStatement("Select * FROM cidade WHERE id=?");
+            this.pstmRall = this.conn.prepareStatement("Select * FROM cidade");
+            this.pstmU = this.conn.prepareStatement("UPDATE cidade SET nome=?, estado=?, pais=?, populacao=? WHERE id=?");
+            this.pstmD = this.conn.prepareStatement("DELETE FROM cidade WHERE id=?");
+        } 
+            catch (Exception ex) {
             ex.printStackTrace();
         }
-        return null;
     }
-
+    public void close(){
+        try{
+            this.conn.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public boolean create(Cidade cidade) {
         try {
-
-            Connection conn = getConnection();
-            String sql = "INSERT INTO Cidade (id,nome,estado,pais,populacao) values (?,?,?,?,?)";
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            pstm.setLong(1, cidade.getId());
-            pstm.setString(2, cidade.getNome());
-            pstm.setString(3, cidade.getEstado());
-            pstm.setString(4, cidade.getPais());
-            pstm.setInt(5, cidade.getPopulacao());
-
-            pstm.executeUpdate();
-            pstm.close();
-            conn.close();
+            this.pstmC.setString(1, cidade.getNome());
+            this.pstmC.setString(2, cidade.getEstado());
+            this.pstmC.setString(3, cidade.getPais());
+            this.pstmC.setInt(4, cidade.getPopulacao());
+            this.pstmC.executeUpdate();
             return true;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -57,41 +64,28 @@ public class DataAccessObjectDB implements DataAccessObject{
     @Override
     public Cidade read(Long id) {
         try {
-            Connection conn = getConnection();
-            String sql = "Select * FROM cidade WHERE id=?";
             Cidade cdd = new Cidade();
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            pstm.setLong(1, id);
-            ResultSet rs = pstm.executeQuery();
-
+            pstmR.setLong(1, id);
+            ResultSet rs = pstmR.executeQuery();
             if (!rs.next()) {
                 return null;
             }
-            cdd.setId(id);
+            cdd.setId(rs.getLong("id"));
             cdd.setNome(rs.getString("nome"));
             cdd.setEstado(rs.getString("estado"));
             cdd.setPais(rs.getString("pais"));
             cdd.setPopulacao(rs.getInt("populacao"));
-            rs.close();
-            pstm.close();
-            conn.close();
-
             return cdd;
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
     }
-
     @Override
     public ArrayList<Cidade> readAll() {
-        try {
-            Connection conn = getConnection();
-            String sql = "Select * FROM cidade";
+         try {
             ArrayList<Cidade> list = new ArrayList<>();
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            ResultSet rs = pstm.executeQuery();
+            ResultSet rs = pstmRall.executeQuery();
             while (rs.next()) {
                 Cidade cdd = new Cidade();
                 cdd.setId(rs.getLong("id"));
@@ -101,9 +95,6 @@ public class DataAccessObjectDB implements DataAccessObject{
                 cdd.setPopulacao(rs.getInt("populacao"));
                 list.add(cdd);
             }
-            rs.close();
-            pstm.close();
-            conn.close();
             return list;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -112,20 +103,17 @@ public class DataAccessObjectDB implements DataAccessObject{
     }
     @Override
     public boolean update(Cidade cidade) {
-        try {
-            Connection conn = getConnection();
-            String sql = "UPDATE cidade SET nome=?, estado=?, pais=?, populacao=? WHERE id=?";
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            pstm.setString(1, cidade.getNome());
-            pstm.setString(2, cidade.getEstado());
-            pstm.setString(3, cidade.getPais());
-            pstm.setInt(4, cidade.getPopulacao());
-            pstm.setLong(5, cidade.getId());
-            pstm.executeUpdate();
-            pstm.close();
-            conn.close();
+           try {
+            this.pstmU.setString(1, cidade.getNome());
+            this.pstmU.setString(2, cidade.getEstado());
+            this.pstmU.setString(3, cidade.getPais());
+            this.pstmU.setInt(4, cidade.getPopulacao());
+            this.pstmU.setLong(5, cidade.getId());
+            
+            this.pstmU.executeUpdate();
+
             return true;
-        } catch (Exception ex) {
+        }catch (Exception ex) {
             ex.printStackTrace();
         }
 
@@ -133,12 +121,10 @@ public class DataAccessObjectDB implements DataAccessObject{
     }
     @Override
     public boolean delete(Cidade cidade) {
-        Connection conn = getConnection();
-        String sql = "DELETE FROM cidade WHERE id=?";
         try {
-            PreparedStatement pstm = conn.prepareStatement(sql);
-            pstm.setLong(1, cidade.getId());
-            pstm.executeUpdate();
+            this.pstmD.setLong(1, cidade.getId());
+            this.pstmD.executeUpdate();
+            return true;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
